332dfb3a1053e9060239c54257dec365928b84ea
Niklas Sombert <niklas@ytvwld.de>
2022-03-31 21:30:11 +0200

    Use pytest.mark.parametrize

--- tests/server/test_state_machine.py
+++ tests/server/test_state_machine.py
@@ -6,14 +6,16 @@ def state_machine():
     return StateMachine()
 
 
-def test_set_get_del(state_machine):
+@pytest.mark.parametrize("key", ["foo", "foo bar", "fööbär", "", "😊"])
+@pytest.mark.parametrize("value", [42, "abcdef", None, True, 1.2])
+def test_set_get_del(state_machine, key, value):
     with pytest.raises(KeyError):
-        state_machine.handle(("get", "foo"))
-    state_machine.handle(("set", "foo", 42))
-    assert state_machine.handle(("get", "foo")) == 42
-    state_machine.handle(("del", "foo"))
+        state_machine.handle(("get", key))
+    state_machine.handle(("set", key, value))
+    assert state_machine.handle(("get", key)) == value
+    state_machine.handle(("del", key))
     with pytest.raises(KeyError):
-        state_machine.handle(("get", "foo"))
+        state_machine.handle(("get", key))
 
 
 def test_set_change(state_machine):
@@ -35,8 +37,7 @@ def test_invalid(state_machine):
         state_machine.handle("invalid")
 
 
-def test_not_found(state_machine):
+@pytest.mark.parametrize("command", ["get", "del"])
+def test_not_found(state_machine, command):
     with pytest.raises(KeyError):
-        state_machine.handle(("get", "nonexisting"))
-    with pytest.raises(KeyError):
-        state_machine.handle(("del", "nonexisting"))
+        state_machine.handle((command, "nonexisting"))
